bd927fb57569ee7f3158ab4eadb31ce2d839c191
hugobarbosa <hugodu38@icloud.com>
2023-07-26 15:34:08 +0200

    map

--- app/src/main/java/com/example/agenda/EventEditActivity.kt
+++ app/src/main/java/com/example/agenda/EventEditActivity.kt
@@ -7,27 +7,40 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import java.time.LocalDate
 import com.google.android.gms.maps.MapView
 import java.time.LocalTime
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class EventEditActivity : AppCompatActivity() {
+
+class EventEditActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private lateinit var eventNameET: EditText
     private lateinit var eventDate: DatePicker
     private lateinit var eventTime: TimePicker
+    private lateinit var mapView: SupportMapFragment
+    private lateinit var googleMap: GoogleMap
 
-    private lateinit var mapView: MapView
     private lateinit var eventDetailActivity: EventDetailsActivity
-
     private var time: LocalTime? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_edit)
+
         eventDetailActivity = EventDetailsActivity()
         initWidgets()
         time = LocalTime.now()
         eventDate.updateDate(CalendarUtils.selectedDate!!.year, CalendarUtils.selectedDate!!.monthValue, CalendarUtils.selectedDate!!.dayOfMonth)
+        mapView = supportFragmentManager
+            .findFragmentById(R.id.map_view) as SupportMapFragment
+        mapView.getMapAsync(this)
+
     }
 
     private fun initWidgets() {
@@ -35,7 +48,33 @@ class EventEditActivity : AppCompatActivity() {
         eventDate = findViewById(R.id.eventDate)
         eventTime = findViewById(R.id.eventTime)
         eventTime.setIs24HourView(true)
-        mapView = findViewById(R.id.mapView)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        val location = LatLng(48.858844, 2.294351)
+        googleMap.addMarker(MarkerOptions().position(location).title("Tour Eiffel"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
     fun saveEventAction(view: View) {
