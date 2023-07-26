37ea71aad520ebc0833c9861d87c293a3985c67d
vfabiano <vfabiano@esgi.fr>
2023-07-26 14:45:38 +0200

    modification formulaire

--- app/src/main/java/com/example/agenda/EventEditActivity.kt
+++ app/src/main/java/com/example/agenda/EventEditActivity.kt
@@ -2,16 +2,20 @@ package com.example.agenda
 
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDate
 import com.google.android.gms.maps.MapView
 import java.time.LocalTime
 
 class EventEditActivity : AppCompatActivity() {
     private lateinit var eventNameET: EditText
-    private lateinit var eventDateTV: TextView
-    private lateinit var eventTimeTV: TextView
+    private lateinit var eventDate: DatePicker
+    private lateinit var eventTime: TimePicker
+
     private lateinit var mapView: MapView
     private lateinit var eventDetailActivity: EventDetailsActivity
 
@@ -23,20 +27,20 @@ class EventEditActivity : AppCompatActivity() {
         eventDetailActivity = EventDetailsActivity()
         initWidgets()
         time = LocalTime.now()
-        eventDateTV.text = "Date: ${CalendarUtils.formattedDate(CalendarUtils.selectedDate)}"
-        eventTimeTV.text = "Time: ${CalendarUtils.formattedTime(time)}"
+        eventDate.updateDate(CalendarUtils.selectedDate!!.year, CalendarUtils.selectedDate!!.monthValue, CalendarUtils.selectedDate!!.dayOfMonth)
     }
 
     private fun initWidgets() {
         eventNameET = findViewById(R.id.eventNameET)
-        eventDateTV = findViewById(R.id.eventDateTV)
-        eventTimeTV = findViewById(R.id.eventTimeTV)
+        eventDate = findViewById(R.id.eventDate)
+        eventTime = findViewById(R.id.eventTime)
+        eventTime.setIs24HourView(true)
         mapView = findViewById(R.id.mapView)
     }
 
     fun saveEventAction(view: View) {
         val eventName = eventNameET.text.toString()
-        val newEvent = Event(eventName, CalendarUtils.selectedDate, time)
+        val newEvent = Event(eventName, LocalDate.of(eventDate.year, eventDate.month, eventDate.dayOfMonth), LocalTime.of(eventTime.hour, eventTime.minute))
         Event.eventsList.add(newEvent)
         finish()
     }
